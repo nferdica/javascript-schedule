60a3974e4e754fb9568eb62f4be00d8bcd5842fd
Nathan Ferdinando <nferdica@outlook.com>
2024-08-03 01:46:54 -0300

    Adding flash messages and validating login with database

--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose')
 
-const homeSchema = new mongoose.Schema({
-    titulo: {type: String, required: true},
-    descricao: String
-})
-
-const homeModel = mongoose.model('Home', homeSchema);
